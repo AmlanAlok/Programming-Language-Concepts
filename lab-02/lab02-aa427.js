e3623455d9adf6a169012fa76055bf8fb4f4a688
Amlan Alok <amlanalok@gmail.com>
2022-07-07 02:35:48 -0500

    Q5

--- lab-02/lab02-aa427.js
+++ lab-02/lab02-aa427.js
@@ -1,12 +1,12 @@
 // const inputtable  = [1,2,3,4,5,6,7,8,9,10]
 // console.log()
 
-console.log('----------------- Q1')
+console.log('------------------------------- Q1')
 const n1 = 10
 const inputtable = [...Array(n1).keys()].map(x => x+1)
 console.log(inputtable)
 
-console.log('----------------- Q2')
+console.log('------------------------------- Q2')
 const fiveTable = inputtable.map(x => x*5)
 console.log(fiveTable)
 
@@ -16,7 +16,7 @@ console.log(thirteenTable)
 const squaresTable = inputtable.map(x => x*x)
 console.log(squaresTable)
 
-console.log('----------------- Q3')
+console.log('------------------------------- Q3')
 const n2 = 20
 const multiplesOfFive = [...Array(n2).keys()].map(x => x+1).map(x => x*5)
 // console.log(multiplesOfFive)
@@ -25,7 +25,7 @@ const multiplesOfFive = [...Array(n2).keys()].map(x => x+1).map(x => x*5)
 const oddFiveMultiples = multiplesOfFive.filter(x => x%2 === 1)
 console.log(oddFiveMultiples)
 
-console.log('----------------- Q4')
+console.log('------------------------------- Q4')
 const n3 = 14
 const multiplesOfSeven = [...Array(n3).keys()].map(x => x+1).map(x => x*7)
 console.log(multiplesOfSeven)
@@ -36,4 +36,19 @@ console.log(evenSevenMultiples)
 const evenSevenMultiplesSum = evenSevenMultiples.reduce((acc, x) => acc+x)
 console.log(`evenSevenMultiplesSum = ${evenSevenMultiplesSum}`)
 
-console.log('----------------- Q5')
+console.log('------------------------------- Q5')
+
+const cylinder_volume = r => h => 3.14*r*r*h
+
+const cylinderRadius5 = cylinder_volume(5)
+
+const volumeWithHeight10 = cylinderRadius5(10)
+console.log(`volumeWithHeight10 = ${volumeWithHeight10}`)
+
+const volumeWithHeight17 = cylinderRadius5(17)
+console.log(`volumeWithHeight17 = ${volumeWithHeight17}`)
+
+const volumeWithHeight11 = cylinderRadius5(11)
+console.log(`volumeWithHeight11 = ${volumeWithHeight11}`)
+
+
