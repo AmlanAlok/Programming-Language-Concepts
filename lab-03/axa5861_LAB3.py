533191853b23688d77031693755f3200caaf053b
Amlan Alok <amlanalok@gmail.com>
2022-07-24 06:58:15 -0500

    made code more efficient

--- lab-03/axa5861_LAB3.py
+++ lab-03/axa5861_LAB3.py
@@ -8,38 +8,16 @@ OS: maxOS Big Sur Version 11.5.1 M1 Chip
 import os
 
 
-def rpn(line):
-    line_data = line.split()
-    # print(line_data)
-    stack = []
-
-    for x in line_data:
-        x_ascii = ord(x)
-
-        if 48 <= x_ascii <= 57:
-            stack.append(int(x))
-        else:
-            b, a = stack.pop(), stack.pop()
-            if x == '+':
-                z = a+b
-            elif x == '*':
-                z = a*b
-            elif x == '-':
-                z = a-b
-            elif x == '/':
-                z = a/b
-            else:
-                print('Invalid operation provided for input =', x)
-                return
-            stack.append(z)
-
-    return stack[0]
-
-
 def main():
 
     current_directory = os.getcwd()
     input_filename = 'input_RPN.txt'
+    operations = {
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a / b
+    }
 
     full_path = os.path.join(current_directory, input_filename)
 
@@ -48,9 +26,28 @@ def main():
             line = f.readline()
             if not line:
                 break
-            ans = rpn(line)
+            ans = rpn(line, operations)
             print(ans)
     f.close()
+
+
+def rpn(line, operations):
+    line_data = line.split()
+    stack = []
+
+    for x in line_data:
+        x_ascii = ord(x)
+
+        if x in operations:
+            b, a = stack.pop(), stack.pop()
+            result = operations[x](a, b)
+            stack.append(result)
+        elif 48 <= x_ascii <= 57:
+            stack.append(int(x))
+        else:
+            print('Invalid operation provided for input =', x)
+            return
+    return stack[0]
 
 
 if __name__ == '__main__':
