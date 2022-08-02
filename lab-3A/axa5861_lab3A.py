147b5a7f0a9ce48304453be07323c081ca65fe4e
Amlan Alok <amlanalok@gmail.com>
2022-08-02 14:21:03 -0500

    lab 3A done

--- lab-3A/axa5861_lab3A.py
+++ lab-3A/axa5861_lab3A.py
@@ -1,3 +1,11 @@
+"""
+Name: Amlan Alok
+StudentID: 1001855861
+Date: 2nd August, 2022
+OS: maxOS Big Sur Version 11.5.1 M1 Chip
+Python Version: 3.9.2
+"""
+
 import random
 import math as m
 
@@ -12,16 +20,24 @@ def tab(space):
 
 def init_program():
     print(tab(28) + 'AMAZING PROGRAM')
-    print(tab(15) + 'CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY')
+    print(tab(15) + 'CREATIVE COMPUTING MORRISTOWN, NEW JERSEY')
     print('\n')
-    # user_input = input('WHAT ARE YOUR WIDTH AND LENGTH? (FOR EXAMPLE TYPE 10,10 AND PRESS ENTER)\n')
-    # width, length = user_input.split(',')
-    # width, length = int(width), int(length)
-    width, length = 10, 10
 
-    print(width, length)
+    # width, length = 10, 10
+
+    # print(width, length)
+    width, length = fetch_user_input()
+    while width <= 1 or length <= 1:
+        print('Meaningles Dimensions. Try Again.')
+        width, length = fetch_user_input()
     return width, length
 
+
+def fetch_user_input():
+    user_input = input('WHAT ARE YOUR WIDTH AND LENGTH? (FOR EXAMPLE TYPE 10,10 AND PRESS ENTER)\n')
+    width, length = user_input.split(',')
+    width, length = int(width), int(length)
+    return width, length
 
 def init_arrays(h, v2):
     w, v = [], []
@@ -69,14 +85,9 @@ def search_non_explored_cell(r, h, s, v2):
     return r, s
 
 
-def main():
-    h, v2 = init_program()
-    w, v = init_arrays(h, v2)
+def maze_logic(x, w, v, h, v2):
 
     q, z = 0, 0
-    x = m.floor(random.random() * h + 1)
-    first_line(x, h)
-
     c = 1
     w[x][1] = c
     c += 1
@@ -192,14 +203,14 @@ def main():
 
         if x == 1:
             w[r-1][s] = c
-            c+=1
+            c += 1
             v[r-1][s] = 2
-            r-=1
+            r -= 1
             if c == h * v2 + 1:
                 break
             q = 0
             entry = 0
-        elif x == 2:        # Up
+        elif x == 2:
             w[r][s-1] = c
             c += 1
             v[r][s-1] = 1
@@ -220,7 +231,7 @@ def main():
                 break
             entry = 1
         elif x == 4:
-            if q != 1:  # Only if not blocked
+            if q != 1:
                 w[r][s + 1] = c
                 c+=1
                 if v[r][s] == 0:
@@ -240,10 +251,10 @@ def main():
                     s = 1
                     while w[r][s] == 0:
                         if r < h:
-                            r+=1
+                            r += 1
                         elif s < v2:
                             r = 1
-                            s+=1
+                            s += 1
                         else:
                             r = 1
                             s = 1
@@ -253,6 +264,10 @@ def main():
                     q = 0
                     entry = 2
 
+    return v
+
+
+def print_maze(v, h, v2):
     j = 1
     while j <= v2:
         st = 'I'
@@ -264,7 +279,6 @@ def main():
                 st += '   '
             i += 1
 
-        # print(st + '\n')
         print(st)
 
         st = ''
@@ -277,7 +291,18 @@ def main():
             i += 1
         print(st + '.')
         j += 1
-    print('END')
+
+
+def main():
+    h, v2 = init_program()
+    w, v = init_arrays(h, v2)
+
+    x = m.floor(random.random() * h + 1)
+    first_line(x, h)
+
+    v = maze_logic(x, w, v, h, v2)
+
+    print_maze(v, h, v2)
 
 
 if __name__ == '__main__':
